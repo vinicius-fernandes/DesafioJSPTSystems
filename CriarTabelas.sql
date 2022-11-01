775bbd39bfd7ae99163e9ca685770b8b47c3fccc
f0fp1422 <viniciusnandes@hotmail.com>
2022-11-01 09:24:50 -0300

    Entidades iniciais

---
+++ CriarTabelas.sql
@@ -0,0 +1,91 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema blogs
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema blogs
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `blogs` DEFAULT CHARACTER SET utf8 ;
+USE `blogs` ;
+
+-- -----------------------------------------------------
+-- Table `blogs`.`TiposUsuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blogs`.`TiposUsuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blogs`.`Usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blogs`.`Usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  `TiposUsuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Usuarios_TiposUsuario_idx` (`TiposUsuario_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Usuarios_TiposUsuario`
+    FOREIGN KEY (`TiposUsuario_id`)
+    REFERENCES `blogs`.`TiposUsuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blogs`.`Postagens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blogs`.`Postagens` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(255) NOT NULL,
+  `corpo` VARCHAR(3000) NOT NULL,
+  `dataCriacao` DATETIME  DEFAULT CURRENT_TIMESTAMP,
+  `UsuarioCriador_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Postagens_Usuarios1_idx` (`UsuarioCriador_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Postagens_Usuarios1`
+    FOREIGN KEY (`UsuarioCriador_id`)
+    REFERENCES `blogs`.`Usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blogs`.`Comentarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blogs`.`Comentarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Postagens_id` INT NOT NULL,
+  `Usuarios_id` INT NOT NULL,
+  `dataCriacao` DATETIME  DEFAULT CURRENT_TIMESTAMP,
+  `aprovado` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Comentarios_Postagens1_idx` (`Postagens_id` ASC) VISIBLE,
+  INDEX `fk_Comentarios_Usuarios1_idx` (`Usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Comentarios_Postagens1`
+    FOREIGN KEY (`Postagens_id`)
+    REFERENCES `blogs`.`Postagens` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Comentarios_Usuarios1`
+    FOREIGN KEY (`Usuarios_id`)
+    REFERENCES `blogs`.`Usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ InsertsIniciais.sql
@@ -0,0 +1,22 @@
+insert into tiposusuario (descricao) values
+('moderador'),
+('comum');
+
+
+insert into usuarios (login,senha,TiposUsuario_id) values
+('vinicius','123abcd',1),
+('ana','123abcd',2);
+
+
+
+insert into postagens (titulo,corpo,UsuarioCriador_id) values
+('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consectetur est nisl, suscipit sagittis ipsum volutpat non',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consectetur est nisl, suscipit sagittis ipsum volutpat non. Sed id mattis risus. Morbi neque libero, malesuada in tortor a, elementum blandit sapien. Maecenas placerat congue ultrices. Suspendisse convallis varius sapien. Vestibulum a risus quam. Proin purus tortor, consectetur et aliquam sed, tincidunt vel enim. In viverra volutpat quam ac tempus. Donec lectus diam, auctor dignissim faucibus vitae, sollicitudin at metus. In ullamcorper a libero vel luctus. Donec euismod scelerisque augue id volutpat. Vestibulum et lacus mauris. Integer ac pretium turpis.
+
+Phasellus eu mauris vel ante mollis tempor. Integer at dapibus risus. Maecenas rhoncus, mauris quis efficitur condimentum, mauris metus ultricies libero, sed tristique risus quam consequat libero. Aliquam id orci sit amet leo semper blandit. Vestibulum eu interdum mi. Vivamus vel mi et est faucibus faucibus. Phasellus vehicula tellus ac commodo convallis. In hac habitasse platea dictumst. Pellentesque ultricies urna et libero gravida, interdum cursus felis iaculis. Nunc eleifend placerat mauris, nec feugiat mauris tincidunt non. Mauris mollis tellus quis libero ultricies ornare. Aliquam sodales magna eget volutpat sagittis. Sed euismod imperdiet dolor vel accumsan. Curabitur tincidunt libero at dui bibendum, ut mollis leo malesuada. Pellentesque eget congue justo. Vestibulum lacinia, urna a sodales rhoncus, diam nunc tristique mi, vitae lobortis neque nisi cursus libero.',
+1),
+('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius ac lacus et euismod',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius ac lacus et euismod. Nam condimentum, ex sit amet imperdiet condimentum, tellus justo ornare eros, quis faucibus nulla lectus ac sem. Suspendisse semper leo ac hendrerit egestas. Mauris mollis, lectus eu porta semper, nisl elit consequat diam, sit amet commodo libero nibh non mi. Suspendisse sagittis lobortis ultrices. Etiam malesuada risus nec turpis sodales vestibulum. Praesent vestibulum metus et justo vulputate, sit amet consectetur ex condimentum. Sed sit amet cursus nibh.
+
+Vivamus massa nulla, porta ornare sapien in, elementum laoreet metus. Nunc venenatis condimentum magna id fringilla. Suspendisse justo nisl, vestibulum id libero tincidunt, blandit varius elit. Maecenas at est sed lacus iaculis sodales. Duis et urna hendrerit, varius nunc mollis, porta purus. Suspendisse aliquam gravida ipsum, non volutpat leo suscipit scelerisque. Etiam felis neque, varius ut luctus rutrum, sodales non odio. Integer ullamcorper feugiat dui vitae suscipit. In elementum blandit purus, et tristique mi. Quisque vitae leo ut metus interdum pellentesque. Maecenas faucibus dictum ullamcorper. Fusce ut condimentum mi, ac sagittis sapien. Vivamus iaculis consectetur odio. In fermentum tortor eu tortor tempor, a sagittis lorem condimentum. Nullam nec leo id ex pretium bibendum.',
+1);
